bc2d6d1915aa70e444ee722f241dc5476c3917b8
Yago Cruz <yaagocruuz@users.noreply.github.com>
2020-03-29 18:08:49 -0300

    initial commit
    - Created main file

---
+++ TripleX.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "You are a secret agent breaking into a secure server room!" << std::endl;
+    std::cout << "Enter the correct code to continue...";
+
+    int a = 0;
+
+    return 0;
+}
